67023c93cfb1e064711be164d171adb7d9d06dd9
Łukasz Dziadek <lukaszdziadek.jaw@gmail.com>
2015-11-02 16:26:41 +0100

    Added EBI data loading.

---
+++ dayTraderHelper/Scripts/ajax/ebiData.js
@@ -0,0 +1,16 @@
+﻿
+
+setInterval(function () {
+
+    var url = "http://www.gpwinfostrefa.pl/GPWIS2/pl/rss/10";
+
+    $.getJSON(document.location.protocol + "//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=1000&callback=?&q=" + encodeURIComponent(url))
+        .success(function (data) {
+            //alert("udalo sie  " + data.responseData.feed.entries[1].title);
+            $("body").append("tytul infa : " + data.responseData.feed.entries[1].title);
+        })
+        .fail(function () {
+            alert("no to mamy problem, nie nawiazalem polaczenia");
+        });
+
+},3000);
