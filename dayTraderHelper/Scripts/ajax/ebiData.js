f25a23d660a4c7dfb60aaa150313b9a10dbfdf88
Łukasz Dziadek <lukaszdziadek.jaw@gmail.com>
2015-11-03 11:00:05 +0100

    Added  rssFeed content to visual part of page.

--- dayTraderHelper/Scripts/ajax/ebiData.js
+++ dayTraderHelper/Scripts/ajax/ebiData.js
@@ -1,16 +1,43 @@
 ﻿
 
+var firstRssDownload = true;
+var firstFeedTitle;
+var newFeedTitle;
+
 setInterval(function () {
 
-    var url = "http://www.gpwinfostrefa.pl/GPWIS2/pl/rss/10";
+    var url = "http://rss.cnn.com/rss/edition_world.rss";
 
     $.getJSON(document.location.protocol + "//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=1000&callback=?&q=" + encodeURIComponent(url))
         .success(function (data) {
             //alert("udalo sie  " + data.responseData.feed.entries[1].title);
-            $("body").append("tytul infa : " + data.responseData.feed.entries[1].title);
+
+
+            if (firstRssDownload) {
+                $.each(data.responseData.feed.entries, function (index, item) {
+                    $("body").append("...." + item.title);
+                    $("body").append("</br>");
+                });
+                $("body").append("</br>");
+                firstFeedTitle = data.responseData.feed.entries[0].title;
+                console.log(firstFeedTitle);
+                firstRssDownload = false;
+            } else {
+                newFeedTitle = data.responseData.feed.entries[0].title;
+
+                if (firstFeedTitle != newFeedTitle) {
+                    $("body").append("----- pojawil sie nowy news ----- </br>");
+                    $("body").append(newFeedTitle);
+                    firstFeedTitle = newFeedTitle;
+                }
+            }
+          
+            //console.log(rssFeeds[0].publishedDate);
+
+
         })
         .fail(function () {
             alert("no to mamy problem, nie nawiazalem polaczenia");
         });
 
-},3000);
+},6000);
