28f114c0e7e1177f7a0004f205624a02b4387073
Łukasz Dziadek <lukaszdziadek.jaw@gmail.com>
2015-11-04 12:25:01 +0100

    Added checking feed title with key words

--- dayTraderHelper/Scripts/ajax/ebiData.js
+++ dayTraderHelper/Scripts/ajax/ebiData.js
@@ -3,6 +3,8 @@
 var firstRssDownload = true;
 var firstFeedTitle;
 var newFeedTitle;
+var newFeedLink;
+var newFeedDate;
 
 setInterval(function () {
 
@@ -11,8 +13,6 @@ setInterval(function () {
     $.getJSON(document.location.protocol + "//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=1000&callback=?&q=" + encodeURIComponent(url))
         .success(function (data) {
             //alert("udalo sie  " + data.responseData.feed.entries[1].title);
-
-
             if (firstRssDownload) {
                 createRssList(data);
                 firstFeedTitle = data.responseData.feed.entries[0].title;
@@ -22,25 +22,41 @@ setInterval(function () {
                 newFeedTitle = data.responseData.feed.entries[0].title;
                 if (firstFeedTitle != newFeedTitle) {
                     alert("nowy feed");
-                    $("#rssList").append("<a class='list-group-item'>" + newFeedTitle + "</a>");
+                    if (checkNewTitle(data, newFeedTitle)) {
+                        newFeedLink = data.responseData.feed.entries[0].link;
+                        newFeedTitle = data.responseData.feed.entries[0].publishedDate;
+                        $("#rssList").append("<a href='" + newFeedLink + " 'class='list-group-item' target='_blank'>"
+                            + "<span class='label label-danger'>" + item.publishedDate + "</span>"
+                            + "<span class='group-item-text info-title'>" + newFeedTitle + "</span>"
+                            + "</a>");
+                    }
                     firstFeedTitle = newFeedTitle;
                 }
             }      
-            //console.log(rssFeeds[0].publishedDate);
         })
         .fail(function () {
             alert("no to mamy problem, nie nawiazalem polaczenia");
         });
 
-}, 6000);
+}, 5000);
 
 
 function createRssList(data) {
     $.each(data.responseData.feed.entries, function (index, item) {
-        $("#rssList").append("<a href='" + item.link + " 'class='list-group-item'>"
+        $("#rssList").append("<a href='" + item.link + " 'class='list-group-item' target='blank'>"
             + "<span class='label label-danger'>" + item.publishedDate + "</span>"
             + "<span class='group-item-text info-title'>" + item.title + "</span>"
             + "</a>");
     });
 
 };
+
+function checkNewTitle(data, newFeedTitle) {
+    alert(newFeedTitle);
+    if (newFeedTitle.toLowerCase().indexOf("a") >= 0) {
+        alert("wiadomosc zawiera szukany klucz a");
+        return true;
+    }
+    return false;
+};
+
