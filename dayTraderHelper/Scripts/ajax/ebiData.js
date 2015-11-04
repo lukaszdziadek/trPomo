1233e9dacb4359f9ed5d2df26e4c1f7df772d137
Łukasz Dziadek <lukaszdziadek.jaw@gmail.com>
2015-11-04 10:14:05 +0100

    Refactoring : added function for generating visual list

--- dayTraderHelper/Scripts/ajax/ebiData.js
+++ dayTraderHelper/Scripts/ajax/ebiData.js
@@ -6,7 +6,7 @@ var newFeedTitle;
 
 setInterval(function () {
 
-    var url = "http://rss.cnn.com/rss/edition_world.rss";
+    var url = "http://biznes.pap.pl/pl/rss/6614?nocache="+ (new Date).getTime();
 
     $.getJSON(document.location.protocol + "//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=1000&callback=?&q=" + encodeURIComponent(url))
         .success(function (data) {
@@ -14,30 +14,33 @@ setInterval(function () {
 
 
             if (firstRssDownload) {
-                $.each(data.responseData.feed.entries, function (index, item) {
-                    $("body").append("...." + item.title);
-                    $("body").append("</br>");
-                });
-                $("body").append("</br>");
+                createRssList(data);
                 firstFeedTitle = data.responseData.feed.entries[0].title;
                 console.log(firstFeedTitle);
                 firstRssDownload = false;
             } else {
                 newFeedTitle = data.responseData.feed.entries[0].title;
-
                 if (firstFeedTitle != newFeedTitle) {
-                    $("body").append("----- pojawil sie nowy news ----- </br>");
-                    $("body").append(newFeedTitle);
+                    alert("nowy feed");
+                    $("#rssList").append("<a class='list-group-item'>" + newFeedTitle + "</a>");
                     firstFeedTitle = newFeedTitle;
                 }
-            }
-          
+            }      
             //console.log(rssFeeds[0].publishedDate);
-
-
         })
         .fail(function () {
             alert("no to mamy problem, nie nawiazalem polaczenia");
         });
 
-},6000);
+}, 6000);
+
+
+function createRssList(data) {
+    $.each(data.responseData.feed.entries, function (index, item) {
+        $("#rssList").append("<a href='" + item.link + " 'class='list-group-item'>"
+            + "<span class='label label-danger'>" + item.publishedDate + "</span>"
+            + "<span class='group-item-text info-title'>" + item.title + "</span>"
+            + "</a>");
+    });
+
+};
